33e3be0065f5123334241e7b03b8e1245262fe13
Pedro Vasconcelos <151564394+Pedro-H-Avila@users.noreply.github.com>
2024-07-08 09:00:28 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        ListaSE numeros = new ListaSE();
-    
+        
         Celula c1 = new Celula(10);
         Celula c2 = new Celula(20);
         Celula c3 = new Celula(30);
 
+        ListaSE numeros = new ListaSE();
+    
         numeros.inserirNoInicio(c1);
         numeros.inserirAoFinal(c2);
         numeros.inserirNoInicio(c3);
