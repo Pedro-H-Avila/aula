0a788d6fa791c3ce4a39a5fbf60b999e4502338a
Pedro Vasconcelos <151564394+Pedro-H-Avila@users.noreply.github.com>
2024-07-11 08:39:34 -0300

    Update ListaSEC.java

--- trabalho/ListaSEC.java
+++ trabalho/ListaSEC.java
@@ -1,18 +1,25 @@
 public class ListaSEC {
-    Celula = head;
+    Celula head;
     
     public ListaSEC () {
         head = new Celula(-1);
-        head.prox = cabeça;
+        head.prox = head;
     }
     public boolean empty(){
         return (head.prox == head);
     }
-    public void inserirNoFinal ( Celula c) [
+    public void inserirNoFinal ( Celula c) {
         if (empty()) {
             head.prox = c;
             c.prox = head;
+         }else {
+            Celula aux = head.prox;
+            while (aux.prox != head){
+                aux = aux.prox;
+            }
+            aux.prox = c;
+            c.prox = head;
          }
-    ]
+    }
 }
 
